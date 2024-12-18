0b0e2d93035028300d6afbf6131117034318a8bc
ksairos <ksairos.dev@gmail.com>
2024-12-18 14:40:57 +0900

    Added JWT Token access and oauth2 authentication

---
+++ src/auth/hashing.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Hash the user password
+def hash_fn(password: str) -> str:
+    return pwd_context.hash(password)
+
+# Verify that the input password is the same with hashed from DB
+def verify(raw_password, hashed_password) -> bool:
+    return pwd_context.verify(raw_password, hashed_password)
