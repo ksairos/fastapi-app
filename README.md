bfa2d29727a4aa2ca11131724f9ec9f974fefec0
ksairos <ksairos.dev@gmail.com>
2025-01-14 18:45:15 +0900

    add requirements.txt and update readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# FastAPI
+# Python API Development using FastAPI
+
+Repository for a FastAPI project based on [[this YouTube course]](https://www.youtube.com/watch?v=0sOvCWFmrtA)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .db.database import engine
 from .db.models import Base
@@ -7,10 +7,20 @@ from .routers import posts, users, auth, vote
 
 ## Use Alembic instead of create_all()
 # Base.metadata.create_all(bind=engine)
-
 app = FastAPI(
     title="My FastAPI Application",
     version="0.0.1", )
+
+# Setting up CORS
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
